b8ad5609b46e202573f25f05970107701c6c4009
Kentaro Hayashi <hayashi@clear-code.com>
2023-12-07 18:27:20 +0900

    Add expected result

--- test-lxc.sh
+++ test-lxc.sh
@@ -6,6 +6,12 @@ set -x
 lxc launch $1 target
 lxc config device add target host disk source=$PWD path=/host
 lxc list
+# + lxc list
+# +--------+---------+------+---------------------------------------------+-----------+-----------+
+# |  NAME  |  STATE  | IPV4 |                    IPV6                     |   TYPE    | SNAPSHOTS |
+# +--------+---------+------+---------------------------------------------+-----------+-----------+
+# | target | RUNNING |      | fd42:60b:f54:e1b4:216:3eff:fe1c:83e3 (eth0) | CONTAINER | 0         |
+# +--------+---------+------+---------------------------------------------+-----------+-----------+
 lxc exec target -- /host/test-apt.sh
 lxc stop target
 lxc delete target
