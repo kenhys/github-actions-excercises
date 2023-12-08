76c384c64d8352068c3f71268696eb0d42db35a8
Kentaro Hayashi <hayashi@clear-code.com>
2023-12-08 11:36:24 +0900

    Add apt/yum switch

--- test-lxc.sh
+++ test-lxc.sh
@@ -4,6 +4,7 @@ set -x
 
 #lxc-checkconfig
 lxc launch $1 target
+sleep 5
 lxc config device add target host disk source=$PWD path=/host
 lxc list
 # + lxc list
@@ -12,6 +13,13 @@ lxc list
 # +--------+---------+------+---------------------------------------------+-----------+-----------+
 # | target | RUNNING |      | fd42:60b:f54:e1b4:216:3eff:fe1c:83e3 (eth0) | CONTAINER | 0         |
 # +--------+---------+------+---------------------------------------------+-----------+-----------+
-lxc exec target -- /host/test-apt.sh
+case $1 in
+    *debian*|*ubuntu*)
+	lxc exec target -- /host/test-apt.sh
+	;;
+    *rocky*|*alma*)
+	lxc exec target -- /host/test-yum.sh
+	;;
+esac
 lxc stop target
 lxc delete target
